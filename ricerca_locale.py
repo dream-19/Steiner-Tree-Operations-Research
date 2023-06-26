1a71c6944c8f56e65d954de56b69e805951488b5
Lucia Ferrari <lucia02.ferrari@edu.unife.it>
2023-06-26 16:54:29 +0200

    risultati ls belli

--- ricerca_locale.py
+++ ricerca_locale.py
@@ -153,3 +153,134 @@ def local_search(original, solution, cost):
             best_solution.draw_graph()
             plt.title("Ho trovato una soluzione migliore!")'''
             
+'''
+Esploro tutto l'intorno, una volta che ho esplorato tutto l'intorno mi fermo solo se la soluzione migliore
+trovata fino a quel momento è uguale a quella iniziale
+
+'''
+def local_search_best_improvment(original, solution, cost):
+    start_time = time.time()
+    k = 1 #numero di esplorazioni 
+    best_solution = solution #migliore soluzione trovata fino a questo momento
+    best_solution_cost = cost #costo della migliore soluzione trovata fino a questo momento
+    all_edges = solution.get_edges()
+    edges_unique = remove_duplicate(all_edges)
+    found_a_best_solution = False #Se nell'intorno ho trovato una soluzione migliore
+    
+    #Durante l'esplorazione dello stesso intorno ho bisogno di operare sempre sullo stesso grafo (non come prima che ogni volta aggiornavo con il best)
+    grafo_su_cui_operare = copy.deepcopy(solution)
+    #Finché trovo una soluzione migliore
+    while True:
+
+        #Se ho esplorato tutto l'intorno senza trovare una soluzione migliore mi fermo
+        if len(edges_unique) == 0 and found_a_best_solution == False :
+            end_time = time.time()
+            execution_time = end_time - start_time
+            return best_solution, best_solution_cost, k, execution_time
+        
+        #Se ho trovato una soluzione migliore in tutto l'intorno allora mi sposto e continuo la ricerca
+        if len(edges_unique) == 0 and found_a_best_solution == True :
+            all_edges = best_solution.get_edges()
+            edges_unique = remove_duplicate(all_edges)
+            k = k + 1
+            found_a_best_solution = False #Ripristino il valore
+            grafo_su_cui_operare = copy.deepcopy(best_solution) #Nuovo grafo su cui esplorare l'intorno
+        
+        ##############################################################################################################
+        #1) Fase di rimozione dell'arco 
+        # Prendo un arco dall'elenco degli archi
+        removed_edge = edges_unique.pop() 
+        first_node_tree1 = removed_edge[0]
+        first_node_tree2 = removed_edge[1]
+        #print("\n\nIterazione:",k,"Rimuovo questo arco: ", removed_edge, first_node_tree1, first_node_tree2)
+        
+        #Inizio a creare la nuova soluzione
+        new_solution = copy.deepcopy(grafo_su_cui_operare) #deep copy dell'oggetto iniziale
+        new_solution.remove_edge(removed_edge)
+        new_solution_edges = remove_duplicate(new_solution.get_edges())
+        
+        #Definizione dei vertici dei due sottografi creati dal taglio
+        grafo1 = []
+        grafo2 = []
+        grafo1.append(first_node_tree1) 
+        grafo2.append(first_node_tree2)
+        
+        #Inserisco quali nodi sono del sottoalbero 1 e quali del sottoalbero2
+        while len(new_solution_edges) > 0:
+            for el in new_solution_edges:
+                found = False
+                if el[0] in grafo1 and el[1] not in grafo1:
+                    grafo1.append(el[1])
+                    found = True
+                    
+                if el[1] in grafo1 and el[0] not in grafo1:
+                    grafo1.append(el[0])
+                    found = True
+                
+                if el[0] in grafo2 and el[1] not in grafo2:
+                    grafo2.append(el[1])
+                    found = True
+                if el[1] in grafo2 and el[0] not in grafo2:
+                    grafo2.append(el[0])
+                    found = True
+                    
+                if found:
+                    new_solution_edges.remove(el)
+                    
+        #print("Vertici Sottografo 1: ", grafo1)
+        #print("Vertici Sottografo 2: ", grafo2)
+       
+        
+        #Rimuovo, finché ci sono o si creano, tutti i nodi di grado 1 non di steiner
+        while True:
+            if not(new_solution.remove_degree_one_nodes()):
+                break
+
+        #Aggiorno di conseguenza i due sottografi
+        insieme1 = set(new_solution.get_vertices())
+        grafo1 = [elemento for elemento in grafo1 if elemento in insieme1]
+        grafo2 = [elemento for elemento in grafo2 if elemento in insieme1]
+        #print("Vertici sottografo 1 aggiornati: ", grafo1)
+        #print("Vertici sottografo 2 aggiornati: ", grafo2)
+        
+        
+        ##############################################################################################################
+        #2) Shortest Path tra tutti i nodi del grafo1 e tutti i nodi del grafo2 (djkstra)
+        
+        #Cerco di chiamare djkstra il minor numero di volte possibile
+        if len(grafo1) > len(grafo2):
+            grafo2, grafo1 = grafo1, grafo2
+            
+        #Calcolo delle distanze
+        possible_paths = {}
+        for nodo in grafo1:
+            _ , nodo_arrivo, distanza = dijkstra(original, nodo, grafo2)
+            possible_paths[nodo] = {distanza: nodo_arrivo}
+        #print("Ho calcolato questi possibili collegamenti tra i due sottografi: ", possible_paths)
+        
+        # Calcolo qual è il percoso minore, tra quelli possibili, che devo seguire per ricollegare i due grafi
+        key_func = lambda item: min(item[1].keys())
+        partenza = min(possible_paths.items(), key=key_func)[0]
+        distanza, arrivo = next(iter(possible_paths[partenza].items()))
+        #print("percorso minore, partenza, arrivo e distanza: ",partenza, arrivo, distanza)
+        
+        #Trovo il collegamento e lo aggiungo formando la nuova soluzione
+        collegamento = original.find_shortest_path(partenza, arrivo)
+        #print("collegamento aggiunto: ", collegamento)
+        #Aggiungo il percorso al grafo di steiner
+        for edge in collegamento:
+            new_solution.add_edge(edge[0],edge[1],collegamento[edge])
+        
+        ##############################################################################################################
+        #3) Calcolo il costo della nuova soluzione e vedo se è una soluzione migliore
+        # Non accetto soluzioni di costo equivalente, rischio di creare loop
+        new_cost = new_solution.calculate_cost()
+        #print("new_cost: ", new_cost)
+        if new_cost < best_solution_cost and check_admissibility(original, new_solution):
+            found_a_best_solution = True
+            best_solution = copy.deepcopy(new_solution)
+            best_solution_cost = new_cost
+            
+            
+
+            
